2651aae11d742b68120c6a3a45aea715c791d7b1
a-szadurski <szadurski.arkadiusz@gmail.com>
2021-04-08 18:01:57 +0200

    display donation form, protect from null in index quantityTotal

--- src/main/java/pl/coderslab/charity/dto/CategoryDto.java
+++ src/main/java/pl/coderslab/charity/dto/CategoryDto.java
@@ -1,4 +1,28 @@
 package pl.coderslab.charity.dto;
 
+import lombok.Data;
+import pl.coderslab.charity.model.Category;
+import pl.coderslab.charity.model.Institution;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Data
 public class CategoryDto {
+
+    private Long id;
+
+    private Integer quantity;
+    private String street;
+    private String city;
+    private String zipCode;
+    private LocalDate pickupDate;
+    private LocalTime pickupTime;
+    private String pickupComment;
+
+    private List<Category> categories;
+
+    private Institution institution;
+
 }
---
+++ src/main/java/pl/coderslab/charity/service/ICategoryService.java
@@ -0,0 +1,7 @@
+package pl.coderslab.charity.service;
+
+public interface ICategoryService {
+
+
+
+}
