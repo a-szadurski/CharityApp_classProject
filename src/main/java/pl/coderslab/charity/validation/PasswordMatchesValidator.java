0f1767962b96c95860fb8b0b50aa1c8271434b9e
a-szadurski <szadurski.arkadiusz@gmail.com>
2021-04-13 15:51:55 +0200

    preliminary security implementation

---
+++ src/main/java/pl/coderslab/charity/validation/PasswordMatchesValidator.java
@@ -0,0 +1,21 @@
+package pl.coderslab.charity.validation;
+
+import pl.coderslab.charity.dto.UserDto;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+    @Override
+    public void initialize(final PasswordMatches constraintAnnotation) {
+        //
+    }
+
+    @Override
+    public boolean isValid(final Object obj, final ConstraintValidatorContext context) {
+        final UserDto userDto = (UserDto) obj;
+        return userDto.getPassword().equals(userDto.getMatchingPassword());
+    }
+
+}
